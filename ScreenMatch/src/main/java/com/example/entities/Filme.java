b80d1520e3c782fe04f79efdf4c6a3b481018984
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-30 13:25:53 -0300

    feat(/entites/Filme): Filme herda de Titulo

--- ScreenMatch/src/main/java/com/example/entities/Filme.java
+++ ScreenMatch/src/main/java/com/example/entities/Filme.java
@@ -1,76 +1,13 @@
 package com.example.entities;
 
-public class Filme {
-    private String nome;
-    private int anoLancamento;
-    private boolean incluidoPlano;
-    private double somaAvaliacoes;
-    private int totalAvaliacoes;
-    private int duracoesMinutos;
+public class Filme extends Titulo{
+    private String diretor;
 
-    public String getNome() {
-        return nome;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoLancamento() {
-        return anoLancamento;
-    }
-
-    public void setAnoLancamento(int anoLancamento) {
-        this.anoLancamento = anoLancamento;
-    }
-
-    public boolean isIncluidoPlano() {
-        return incluidoPlano;
-    }
-
-    public void setIncluidoPlano(boolean incluidoPlano) {
-        this.incluidoPlano = incluidoPlano;
-    }
-
-    public double getSomaAvaliacoes() {
-        return somaAvaliacoes;
-    }
-    
-
-    public int getTotalAvaliacoes() {
-        return totalAvaliacoes;
-    }
-
-    public void setTotalAvaliacoes(int totalAvaliacoes) {
-        this.totalAvaliacoes = totalAvaliacoes;
-    }
-
-    public int getDuracoesMinutos() {
-        return duracoesMinutos;
-    }
-
-    public void setDuracoesMinutos(int duracoesMinutos) {
-        this.duracoesMinutos = duracoesMinutos;
-    }
-
-    public void exibeFichaTecnica(){
-        System.out.printf(
-                """
-                Nome do filme: %s
-                Ano de lançamento: %d
-                Duração em minutos: %d
-                Incluído no lano: $b
-                """
-                ,nome, anoLancamento, duracoesMinutos, incluidoPlano
-        );
-    }
-
-    public void avalia(double nota) {
-            somaAvaliacoes += nota;
-            totalAvaliacoes++;
-        }
-
-    public double pegaMedia(){
-        return somaAvaliacoes / totalAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
