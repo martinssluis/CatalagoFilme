1e6be7471425a29c7e6913d9330fc0de12fd11b2
Luis Martins <luishenrique.martinslima@gmail.com>
2026-05-30 12:52:52 -0300

    feat(Main): cria instancia de Filme

--- ScreenMatch/src/main/java/com/example/Main.java
+++ ScreenMatch/src/main/java/com/example/Main.java
@@ -1,18 +1,19 @@
 package com.example;
 
+import com.example.entities.Filme;
+
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Poderoso chefão";
-        meuFilme.anoLancamento = 1970;
-        meuFilme.duracoesMinutos = 180;
+        meuFilme.setNome("O Poderoso chefão");
+        meuFilme.setAnoLancamento(1970);
+        meuFilme.setDuracoesMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println(meuFilme.somaAvaliacoes);
-        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
     }
 }
--- ScreenMatch/src/main/java/com/example/entities/Filme.java
+++ ScreenMatch/src/main/java/com/example/entities/Filme.java
@@ -1,35 +1,76 @@
-package com.example;
+package com.example.entities;
 
 public class Filme {
-    String nome;
-    int anoLancamento;
-    boolean incluidoPlano;
+    private String nome;
+    private int anoLancamento;
+    private boolean incluidoPlano;
     private double somaAvaliacoes;
     private int totalAvaliacoes;
-    int duracoesMinutos;
+    private int duracoesMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public boolean isIncluidoPlano() {
+        return incluidoPlano;
+    }
+
+    public void setIncluidoPlano(boolean incluidoPlano) {
+        this.incluidoPlano = incluidoPlano;
+    }
+
+    public double getSomaAvaliacoes() {
+        return somaAvaliacoes;
+    }
+    
 
     public int getTotalAvaliacoes() {
         return totalAvaliacoes;
     }
 
-    void exibeFichaTecnica(){
-        System.out.println(String.format(
+    public void setTotalAvaliacoes(int totalAvaliacoes) {
+        this.totalAvaliacoes = totalAvaliacoes;
+    }
+
+    public int getDuracoesMinutos() {
+        return duracoesMinutos;
+    }
+
+    public void setDuracoesMinutos(int duracoesMinutos) {
+        this.duracoesMinutos = duracoesMinutos;
+    }
+
+    public void exibeFichaTecnica(){
+        System.out.printf(
                 """
                 Nome do filme: %s
                 Ano de lançamento: %d
                 Duração em minutos: %d
                 Incluído no lano: $b
                 """
-                ,nome, anoLancamento, duracoesMinutos, incluidoPlano)
+                ,nome, anoLancamento, duracoesMinutos, incluidoPlano
         );
     }
 
-        void avalia(double nota) {
+    public void avalia(double nota) {
             somaAvaliacoes += nota;
             totalAvaliacoes++;
         }
 
-    double pegaMedia(){
+    public double pegaMedia(){
         return somaAvaliacoes / totalAvaliacoes;
     }
 }
