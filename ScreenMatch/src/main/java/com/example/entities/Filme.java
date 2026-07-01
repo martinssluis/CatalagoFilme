fe37d6b0f78c1f38984fbe34a9b3868aa060ac04
Luis <martinsluisdev@gmail.com>
2026-07-01 09:06:45 -0300

    feat(Filme): cria construtor com parametro nome

--- ScreenMatch/src/main/java/com/example/entities/Filme.java
+++ ScreenMatch/src/main/java/com/example/entities/Filme.java
@@ -5,6 +5,10 @@ import com.example.services.Classificavel;
 public class Filme extends Titulo implements Classificavel {
     private String diretor;
 
+    public Filme(String nome){
+        this.setNome(nome);
+    }
+
     public String getDiretor() {
         return diretor;
     }
