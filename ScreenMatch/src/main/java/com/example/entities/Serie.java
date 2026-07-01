e1c562d9d8753f5184ff28df802d0444c5fbcf76
Luis <martinsluisdev@gmail.com>
2026-07-01 10:11:36 -0300

    feat(Serie, Filme): cria construtor com super para referenciar construtor da classe mãe Titulo

--- ScreenMatch/src/main/java/com/example/entities/Serie.java
+++ ScreenMatch/src/main/java/com/example/entities/Serie.java
@@ -6,6 +6,9 @@ public class Serie extends Titulo{
     private int episodiosPorTemporada;
     private int minutosPorEpisodio;
 
+    public Serie(String nome, int anoLancamento) {super(nome, anoLancamento);}
+
+
     public int getTemporadas() {
         return temporadas;
     }
