1a227f3870cbed3b3c39eae47f4c5f81be46486a
Luis <martinsluisdev@gmail.com>
2026-07-08 15:18:00 -0300

    feat(PrincipaÃlComListas): cria nova classe principal com lista parametrizada com super tipo Titulo pára aceitar tanto serie como filme e classifica titulos

--- ScreenMatch/src/main/java/com/example/Principal/PrincipalComListas.java
+++ ScreenMatch/src/main/java/com/example/Principal/PrincipalComListas.java
@@ -1,4 +1,31 @@
 package com.example.Principal;
 
+import com.example.entities.Filme;
+import com.example.entities.Serie;
+import com.example.entities.Titulo;
+
+import java.util.ArrayList;
+
 public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O Poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+
+        for (Titulo item: lista){
+            System.out.println(item.getNome());
+            Filme filme = (Filme) item;
+            System.out.println("Classigicação " + filme.getClassificacao());
+        }
+    }
 }
