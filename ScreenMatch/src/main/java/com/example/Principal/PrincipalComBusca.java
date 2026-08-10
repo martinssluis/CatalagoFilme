7d546160f70abb75bc852c2c9a8a7c8cc902ed4f
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-10 15:44:04 -0300

    feat(PrincipalComBusca): cria Scanner para pegar o nome do filme e ser passado dinamicamente no endereço de busca da api

--- ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
+++ ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um filme para busca: ");
+        var busca = sc.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=2fce2eba";
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=2fce2eba"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
