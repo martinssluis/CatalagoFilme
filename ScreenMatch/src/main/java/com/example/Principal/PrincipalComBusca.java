13d750206b55a139c342b0c75ed87d6cf3d8d3e1
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-11 16:56:22 -0300

    refactor(PrincipalComBusca): em vez de criar apenas um novo Gson, agora builda o Gson no padão UPPER_CAMEL_CASE, cria instancia de meuTituloOmdb, associa instancia de Titulo a instancia de TituloOmdb imprimindo as duas instancias

--- ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
+++ ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
@@ -1,7 +1,10 @@
 package com.example.Principal;
 
+import com.example.dto.TituloOmdb;
 import com.example.entities.Titulo;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
 import java.net.URI;
@@ -28,8 +31,13 @@ public class PrincipalComBusca {
         String json = response.body();
         System.out.println(json);
 
-        Gson gson = new Gson();
-        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+        System.out.println(meuTituloOmdb);
+        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+        System.out.println("Titulo já convertido");
         System.out.println(meuTitulo);
     }
 }
