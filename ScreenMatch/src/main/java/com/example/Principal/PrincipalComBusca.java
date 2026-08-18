d0dbb0ce319faf8a15b38f824b8676ed118d20f4
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-18 14:01:47 -0300

    feat(PrincipalComBusca): cria FileWriter para escrever um filme em um arquivo txt dedicado

--- ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
+++ ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
@@ -7,6 +7,7 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -41,6 +42,11 @@ public class PrincipalComBusca {
             Titulo meuTitulo = new Titulo(meuTituloOmdb);
             System.out.println("Titulo já convertido");
             System.out.println(meuTitulo);
+
+            FileWriter escrita = new FileWriter("filmes.txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
+
         } catch (NumberFormatException e){
             System.out.println("Aconteceu um erro: " + e.getMessage());
         } catch(IllegalArgumentException e){
