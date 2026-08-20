ab570005f9102e358eca3f2781af977f7a0d9968
Luis Martins <luishenrique.martinslima@gmail.com>
2026-08-20 12:07:45 -0300

    feat(PrincipalComBusca): cria loop para adicionar titulos a uma ArrayList de titulos

--- ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
+++ ScreenMatch/src/main/java/com/example/Principal/PrincipalComBusca.java
@@ -13,48 +13,55 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Digite um filme para busca: ");
-        var busca = sc.nextLine();
-        String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=2fce2eba";
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
 
-        try{
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(endereco))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
+        while(!busca.equalsIgnoreCase("sair")) {
+            System.out.print("Digite um filme para busca: ");
+            busca = sc.nextLine();
 
-            String json = response.body();
-            System.out.println(json);
+            if(busca.equalsIgnoreCase("sair")){break;}
 
-            Gson gson = new GsonBuilder()
-                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                    .create();
-            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(meuTituloOmdb);
-            //try{
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println("Titulo já convertido");
-            System.out.println(meuTitulo);
+            String endereco = "https://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=2fce2eba";
 
-            FileWriter escrita = new FileWriter("filmes.txt");
-            escrita.write(meuTitulo.toString());
-            escrita.close();
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-        } catch (NumberFormatException e){
-            System.out.println("Aconteceu um erro: " + e.getMessage());
-        } catch(IllegalArgumentException e){
-            System.out.println("Algum erro de argumento na busca, verifique o endereço. Erro:  " + e.getMessage());
-        } catch (ErroDeConversaoDeAnoException e) {
-            System.out.println(e.getMessage());
+                String json = response.body();
+                System.out.println(json);
+
+                Gson gson = new GsonBuilder()
+                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                        .create();
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
+                //try{
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("Titulo já convertido");
+                System.out.println(meuTitulo);
+                titulos.add(meuTitulo);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Algum erro de argumento na busca, verifique o endereço. Erro:  " + e.getMessage());
+            } catch (ErroDeConversaoDeAnoException e) {
+                System.out.println(e.getMessage());
+            }
         }
-
+        System.out.print(titulos);
         System.out.println("Programa finalizou corretamente!");
     }
 }
